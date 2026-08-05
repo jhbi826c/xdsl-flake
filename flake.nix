8a412f0fa618317006c704ee60bbff1d8bd16c8c
jhbi826c <jiahong.bi@icloud.com>
2026-08-05 22:43:26 +0200

    Init commit

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "xDSL Python environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  outputs = { self, nixpkgs }:
+    let
+      xdslVersion = "0.69.0";
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          python = pkgs.python312.override {
+            packageOverrides = pfinal: pprev: {
+              xdsl = pfinal.buildPythonPackage rec {
+                pname = "xdsl"; version = xdslVersion; pyproject = true;
+                src = pkgs.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-zDZcwcdGvVy/B/NI5smeLahagU917kSpHLnKog6jwWY=";
+                };
+                build-system = [ pfinal.setuptools pfinal.setuptools-scm ];
+                dependencies = [
+                  pfinal.immutabledict
+                  pfinal.ordered-set
+                  pfinal.typing-extensions
+                ];
+                doCheck = false;
+                pythonImportsCheck = [ "xdsl" ];
+              };
+            };
+          };
+          pythonEnv = python.withPackages (ps: with ps; [
+            xdsl
+            pytest
+          ]);
+        in {
+          inherit python pythonEnv;
+          default = pythonEnv;
+        });
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          pythonEnv = self.packages.${system}.pythonEnv;
+        in {
+          default = pkgs.mkShell {
+            packages = [ pythonEnv ];
+            shellHook = ''
+              VENV_DIR=.venv
+              if [ ! -d "$VENV_DIR" ]; then
+                ${pythonEnv}/bin/python -m venv --system-site-packages "$VENV_DIR"
+              fi
+              source "$VENV_DIR/bin/activate"
+            '';
+          };
+        });
+      overlays.default = final: prev: {
+        xdsl-python = self.packages.${final.system}.python;
+        xdsl-env = self.packages.${final.system}.pythonEnv;
+      };
+      nixosModules.default = { pkgs, ... }: {
+        nixpkgs.overlays = [ self.overlays.default ];
+        environment.systemPackages = [ pkgs.xdsl-env ];
+      };
+    };
+}
